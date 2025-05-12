64ea9e83fd1d62ff580b358cf9ab04e34f0b11b5
Polina Haltseva <polinayut@gmail.com>
2025-05-13 01:42:04 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+
+    if not api_key:
+        print(
+            "API_KEY is not set."
+        )
+        exit(1)
+
+    city = "Paris"
+
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        weather = data["current"]["condition"]["text"]
+        temperature = data["current"]["temp_c"]
+
+        print(f"{city} weather: {weather}")
+        print(f"Temperature: {temperature}°C")
+    else:
+        print(f"Error message: {response.status_code}")
 
 
 if __name__ == "__main__":
